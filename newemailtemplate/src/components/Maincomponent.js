c921094fecdd352e690477d8fa7cf66b858449b9
shivamsoni88 <soni.shivam@thinksys.com>
2022-05-23 19:19:44 +0530

    message

--- newemailtemplate/src/components/Maincomponent.js
+++ newemailtemplate/src/components/Maincomponent.js
@@ -9,34 +9,36 @@ const Maincomponent = () => {
   return (
     <div className="middleComponent">
       <div className="middlePart">
-        <div className="startingTime">
-          <div>Select a Starting Time</div>
-          <div className="availableTime">Available Times:</div>
-        </div>
-        <div className="totalCompoPart">
-          <div className="calender">
-            <Calendar onChange={onChange} value={value} />
+        <div className="startingtTimeAndTotalComponentPart">
+          <div className="startingTime">
+            <div className="selectStartingTime">Select a Starting Time</div>
+            <div className="calender">
+              <Calendar onChange={onChange} value={value} />
+            </div>
           </div>
-          {/* {/ <div>Available Times</div> /} */}
-          <div class="grid-container">
-            <div class="grid-item">8:00AM</div>
-            <div class="grid-item"> 8:30AM</div>
-            <div class="grid-item"> 9:00AM</div>
-            <div class="grid-item2"> 9:30AM</div>
-            <div class="grid-item1"> 10:00AM</div>
-            <div class="grid-item1"> 10:30AM</div>
-            <div class="grid-item1">11:00AM</div>
-            <div class="grid-item1">11:30AM</div>
-            <div class="grid-item1">12:00PM</div>
-            <div class="grid-item">1:00PM</div>
-            <div class="grid-item">1:30PM</div>
-            <div class="grid-item3">2:00PM</div>
-            <div class="grid-item">2:30PM</div>
-            <div class="grid-item">3:00PM</div>
-            <div class="grid-item">3:30PM</div>
-            <div class="grid-item">4:00PM</div>
-            <div class="grid-item">4:30PM</div>
-            <div class="grid-item">5:00PM</div>
+          <div className="totalCompoPart">
+            <div className="availableTime">Available Times:</div>
+
+            <div class="grid-container">
+              <div class="grid-item">8:00AM</div>
+              <div class="grid-item"> 8:30AM</div>
+              <div class="grid-item"> 9:00AM</div>
+              <div class="grid-item2"> 9:30AM</div>
+              <div class="grid-item1"> 10:00AM</div>
+              <div class="grid-item1"> 10:30AM</div>
+              <div class="grid-item1">11:00AM</div>
+              <div class="grid-item1">11:30AM</div>
+              <div class="grid-item1">12:00PM</div>
+              <div class="grid-item">1:00PM</div>
+              <div class="grid-item">1:30PM</div>
+              <div class="grid-item3">2:00PM</div>
+              <div class="grid-item">2:30PM</div>
+              <div class="grid-item">3:00PM</div>
+              <div class="grid-item">3:30PM</div>
+              <div class="grid-item">4:00PM</div>
+              <div class="grid-item">4:30PM</div>
+              <div class="grid-item">5:00PM</div>
+            </div>
           </div>
         </div>
         <div className="pickupAddress">Pickup Address(Optional)</div>
@@ -45,14 +47,18 @@ const Maincomponent = () => {
           We need the address of your accomodation in order to arrange pickup
         </div>
 
-        <div className="mobileAndEmail">
-          <div className="mobilePhone">Your mobile phone</div>
-          <div className="mobilePhone1">Your email</div>
-        </div>
+        <div className="xyz">
+          <div className="mobileAndEmail">
+            <div className="mobilePhone">Your mobile phone</div>
+            <div className="inputField1">+370 688 8545</div>
+            {/* <div className="mobilePhone1">Your email</div> */}
+          </div>
 
-        <div className="mobileAndEmail">
-          <div className="inputField1">+370 688 8545</div>
-          <div className="inputfield12">darius@gmail.com</div>
+          <div className="mobileAndEmail">
+            {/* <div className="inputField1">+370 688 8545</div> */}
+            <div className="mobilePhone1">Your email</div>
+            <div className="inputfield12">darius@gmail.com</div>
+          </div>
         </div>
       </div>
       <div className="footer">
